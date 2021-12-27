0bb214838d175f771b3b42087f36de42a4fc2b4e
Hiago Patrick <hiogaptk@gmail.com>
2021-12-27 11:30:57 -0300

    criando a função de verificação de conta existente

--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const existsUserAccount = users.some(user => user.username === username);
+
+  if (!existsUserAccount) {
+    return response.status(400).json({error: 'User not found'})
+  }
+
+  request.username = username;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -28,13 +37,13 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
 
+});
+ 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
